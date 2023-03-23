d63e44cfee1776ad24845b7617a42ab8bf7fcae8
Steve <zsombikisti@gmail.com>
2023-03-23 07:07:16 +0100

    no data structure

--- GYAK/GYAK04/GYAK04.py
+++ GYAK/GYAK04/GYAK04.py
@@ -16,11 +16,11 @@ import numpy as np
 FONTOS: Az első feladatáltal visszaadott DataFrame-et kell használni a további feladatokhoz. 
 A függvényeken belül mindig készíts egy másolatot a bemenő df-ről, (new_df = df.copy() és ezzel dolgozz tovább.)
 '''
-stats = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
-       "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
-       "area": [8.516, 17.10, 3.286, 9.597, 1.221],
-       "population": [200.4, 143.5, 1252, 1357, 52.98] }
-df = dict_to_dataframe(stats)
+# stats = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+#        "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+#        "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+#        "population": [200.4, 143.5, 1252, 1357, 52.98] }
+# df = dict_to_dataframe(stats)
 #get_column(df,"country")
 #get_top_two(df)
 #population_density(df)
