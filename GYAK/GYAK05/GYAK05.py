ebf020e4fe8215ed09b218181f02a3c888181aba
Steve <zsombikisti@gmail.com>
2023-03-30 18:37:38 +0200

    gyak05 fixed

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -8,22 +8,21 @@ from sklearn.metrics import confusion_matrix
 class KNNClassifier:
     
     @property
-    def k_neighbours(self):
+    def k_neighbors(self):
         return self.k
     
     def __init__(self, k:int, test_split_ratio:float):
         self.k = k
-        self.test_split_ration = test_split_ratio
+        self.test_split_ratio = test_split_ratio
         
-    def train_test_split(self,features:np.ndarray,labels:np.ndarray) -> None:
-        
-        test_size = int(len(features) * test_split_ratio)
+    def train_test_split(self, features:np.ndarray, labels:np.ndarray):
+        test_size = int(len(features) * self.test_split_ratio)
         train_size = len(features) - test_size
         assert len(features) == test_size + train_size, "Size mismatch!"
 
-        self.x_train, self.y_train = features[:train_size,:],labels[:train_size]
-        self.x_test, self.y_test = features[train_size:train_size+test_size,:], labels[train_size:train_size + test_size]
-    
+        self.x_train, self.y_train = features[:train_size, :], labels[:train_size]
+        self.x_test, self.y_test = features[train_size:, :], labels[train_size:]
+        
     def euclidean(self, element_of_x:np.ndarray) -> np.ndarray:
         return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis=1))
     
@@ -34,7 +33,7 @@ class KNNClassifier:
             distances = np.array(sorted(zip(distances, self.y_train)))
             label_pred = mode(distances[:self.k,1],keepdims=False).mode
             labels_pred.append(label_pred)
-        self.pred = np.array(labels_pred,dtype=np.int32)
+        self.y_preds = np.array(labels_pred, dtype=np.int64)
     
     def accuracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()
@@ -45,15 +44,9 @@ class KNNClassifier:
         sns.heatmap(conf_matrix,annot=True) 
         
     @staticmethod
-    def load_csv(csv_path:str) -> Tuple[np.ndarray,np.ndarray]:
+    def load_csv(csv_path:str) -> Tuple[np.ndarray, np.ndarray]:
         np.random.seed(42)
-        dataset = np.genfromtxt(csv_path,delimiter=',')
-        np.random.shuffle(dataset,)
-        x,y = dataset[:,:4],dataset[:,-1]
-        x[np.isnan(x)] = 3.5
-        y = np.delete(y,np.where(x < 0.0)[0],axis=0)
-        y = np.delete(y,np.where(x > 13.0)[0],axis=0)
-        x = np.delete(x,np.where(x < 0.0)[0],axis=0)
-        x = np.delete(x,np.where(x > 13.0)[0],axis=0)
+        dataset = np.genfromtxt(csv_path, delimiter=',')
+        np.random.shuffle(dataset)
+        x,y = dataset[:,:-1], dataset[:,-1]
         return x,y
-
