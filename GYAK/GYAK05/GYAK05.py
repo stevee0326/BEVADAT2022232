f84cc1d2ab6d8f99f478910fe7d92a01c23f80a6
Steve <zsombikisti@gmail.com>
2023-04-02 17:32:02 +0200

    f

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -1,20 +1,19 @@
-
 import numpy as np
-import seaborn as sns
+from scipy.stats import mode 
 from typing import Tuple
-from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 class KNNClassifier:
-    
-    @property
-    def k_neighbors(self):
-        return self.k
-    
+
     def __init__(self, k:int, test_split_ratio:float):
         self.k = k
         self.test_split_ratio = test_split_ratio
-        
+
+    @property
+    def k_neighbors(self):
+        return self.k
+
     def train_test_split(self, features:np.ndarray, labels:np.ndarray):
         test_size = int(len(features) * self.test_split_ratio)
         train_size = len(features) - test_size
@@ -22,27 +21,26 @@ class KNNClassifier:
 
         self.x_train, self.y_train = features[:train_size, :], labels[:train_size]
         self.x_test, self.y_test = features[train_size:, :], labels[train_size:]
-        
-    def euclidean(self, element_of_x:np.ndarray) -> np.ndarray:
-        return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis=1))
-    
+
     def predict(self, x_test:np.ndarray):
         labels_pred = []
         for x_test_element in x_test:
-            distances = self.euclidean(self.x_train,x_test_element)
+            distances = self.euclidean(x_test_element)
             distances = np.array(sorted(zip(distances, self.y_train)))
-            label_pred = mode(distances[:self.k,1],keepdims=False).mode
+            label_pred = mode(distances[:self.k,1], keepdims = False).mode
             labels_pred.append(label_pred)
         self.y_preds = np.array(labels_pred, dtype=np.int64)
+
+    def euclidean(self, element_of_x:np.ndarray) -> np.ndarray:
+        return np.sqrt(np.sum((self.x_train - element_of_x)**2, axis=1))
     
     def accuracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()
         return true_positive / len(self.y_test) * 100
     
     def confusion_matrix(self):
-        conf_matrix = confusion_matrix(self.y_test,self.y_preds)
-        sns.heatmap(conf_matrix,annot=True) 
-        
+        return confusion_matrix(self.y_test, self.y_preds)
+    
     @staticmethod
     def load_csv(csv_path:str) -> Tuple[np.ndarray, np.ndarray]:
         np.random.seed(42)
