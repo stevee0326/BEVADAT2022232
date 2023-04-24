b686db9c2c30bdfa848ee3f369675221d45f537e
Steve <zsombikisti@gmail.com>
2023-04-24 19:54:35 +0200

    .

--- GYAK/GYAK08/LinearRegressionSkeleton.py
+++ GYAK/GYAK08/LinearRegressionSkeleton.py
@@ -1,39 +1,29 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
-
 
 class LinearRegression:
     def __init__(self, epochs: int = 1000, lr: float = 1e-3):
         self.epochs = epochs
         self.lr = lr
         self.m = 0 #meredekség
-        self.c = 0 #metszés az x tengellyel
+        self.c = 0 #x metszet
 
     def fit(self, X: np.array, y: np.array):
-        self.X = X
-        self.y = y
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        self.build()
-
-    def build(self):
-        # Building the model
-        n = float(len(self.X_train)) # Number of elements in X
-
-        # Performing Gradient Descent 
-        losses = []
-        for i in range(self.epochs): 
-            y_pred = self.m*self.X_train + self.c  # The current predicted value of Y
-
-            residuals = y_pred - self.y_train #meghatározzuk a távolságot
-            loss = np.sum(residuals ** 2)
-            losses.append(loss)
-            D_m = (-2/n) * sum(self.X_train * residuals)  # Derivative wrt m -> mennyivel kell változtatni m-t
-            D_c = (-2/n) * sum(residuals)  # Derivative wrt c -> mennyivel kell változtatni c-t
-            self.m = self.m + self.lr * D_m  # Update m
-            self.c = self.c + self.lr * D_c  # Update c
-            if i % 100 == 0:
-                print(np.mean(self.y_train-y_pred))
-    
+        n = float(len(X))
+        for i in range(self.epochs):
+            y_pred = self.m * X + self.c    #curr y pred
+            residuals = y - y_pred  #distance
+            
+            #derivatings
+            D_m = (-2/n) * sum(X * residuals)
+            D_c = (-2/n) * sum(residuals)
+            
+            #updatings
+            self.m = self.m - self.lr * D_m
+            self.c = self.c - self.lr * D_c
 
     def predict(self, X):
-        return self.m*X + self.c
+        return self.m * X + self.c
+    
+
+
+
